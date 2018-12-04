0c51021b9839630d232a92ab5e0cf5d0cdf3ae4d
veecher <veecher@gmail.com>
2018-12-03 19:49:12 -0800

    added stuff

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,37 @@
+var newIngredient = "";
+
+var ingredients = [];
+
+var currentUser = "Guinea Pig";
+
+var renameThisApiKeyVariableLater = "first api key";
+
+var renameThisVariableForTheOtherApiKeyLaterToo = "other api key";
+
+function ingredientLister() {
+    // add ingredient to search
+}
+
+function ingredientCounter() {
+    // adds ingredients to database
+}
+
+function popularIngredients() {
+    // gets the most commonly searched for ingredients
+}
+
+function recipeFinder() {
+    // search for recipes
+}
+
+function recipeLister() {
+    // put recipes into #recipe-box
+}
+
+function helloUser() {
+    // simulates logged in user
+}
+
+function recipeSaver() {
+    // for saving favorite recipes
+}
