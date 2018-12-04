23101b3c13e50d5504ff8eaabd9cc784db74ec46
veecher <veecher@gmail.com>
2018-12-03 20:14:39 -0800

    merged js with mikes

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 var newIngredient = "";
 
 var ingredients = [];
@@ -81,12 +81,12 @@ function recipeFinder() {
 // q : normal search query
 // p : page
 // format=xml : if you want xml instead of json 
-=======
+// =======
 // API Key for Edamam below://
-Name: API signup	Key: d02d745b
+// Name: API signup	Key: d02d745b
 
-    < form action = "http://www.recipepuppy.com/" method = "get" style = "font-size:10px;" > 
-    <b style="font-size:14px;">Recipe Search by Ingredients:</b> 
-    <input type="text" name="i" id="i" value="Enter ingredients separated by commas" size=31 style = "color:gray;font-size:11px;" onfocus = "if(this.style.color=='gray'){this.style.color='black';this.value='';}" onblur = "if(this.value==''){this.style.color='gray';this.value='Enter ingredients separated by commas';}" /> 
-    <input type="submit" value="Search" style="font-size:11px;" /> <br />Powered by < a href = "http://www.recipepuppy.com" style = "font-size:10px;" > Recipe Puppy</a ></form >
->>>>>>> origin/mike.newyn
+//     < form action = "http://www.recipepuppy.com/" method = "get" style = "font-size:10px;" > 
+//     <b style="font-size:14px;">Recipe Search by Ingredients:</b> 
+//     <input type="text" name="i" id="i" value="Enter ingredients separated by commas" size=31 style = "color:gray;font-size:11px;" onfocus = "if(this.style.color=='gray'){this.style.color='black';this.value='';}" onblur = "if(this.value==''){this.style.color='gray';this.value='Enter ingredients separated by commas';}" /> 
+//     <input type="submit" value="Search" style="font-size:11px;" /> <br />Powered by < a href = "http://www.recipepuppy.com" style = "font-size:10px;" > Recipe Puppy</a ></form >
+// >>>>>>> origin/mike.newyn
