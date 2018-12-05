8557799ff033af5587918fff3f52b2524a5e3b4b
Michael Nguyen <thinknownotforever@gmail.com>
2018-12-05 13:35:58 -0800

    cleaned up for loop

--- assets/javascript/javascriptNewyn.js
+++ assets/javascript/javascriptNewyn.js
@@ -14,15 +14,17 @@ $(document).on("click", "#submitButton", function () {
         method: "GET"
         })
         .then(function(response) {
-        console.log(response);
-        var results = response.hits;
-        console.log(results[0].recipe.image);
-        var d = $('<div>');
-        var images = $('<img>');
-        images.attr("src", results[0].recipe.image);
-        d.append(images);
-        $("#recipeResults").append(d);
-        
+// To get image to append into the "Let's get cooking" div
+        // console.log(response);
+        // var results = response.hits;
+        // console.log(results[0].recipe.image);
+        // var d = $('<div>');
+        // var images = $('<img>');
+        // images.attr("src", results[0].recipe.image);
+        // d.append(images);
+        // $("#recipeResults").append(d);
+ 
+// For loop to get 10 images into array and append to "Let's get cooking"
         for(var i = 0; i < results.length; i++){
             var d = $('<div>');
             var images = $('<img>');
