bac5f0033b7d1cf4c52d63487809d6c2b675ea34
richard baylis <veecher@gmail.com>
2018-12-05 10:25:49 -0800

    weds morning

---
+++ assets/javascript/baylis.js
@@ -0,0 +1,12 @@
+
+
+
+var config = {
+    apiKey: "AIzaSyBwKeW4YmJyeHvSeCRgQrPvSayDP0yfvWs",
+    authDomain: "whats-in-my-fridge-335ea.firebaseapp.com",
+    databaseURL: "https://whats-in-my-fridge-335ea.firebaseio.com",
+    projectId: "whats-in-my-fridge-335ea",
+    storageBucket: "whats-in-my-fridge-335ea.appspot.com",
+    messagingSenderId: "82405487027"
+};
+firebase.initializeApp(config);
