5f6f049358134d15163ac3c58a80d18819c34cf3
richard baylis <veecher@gmail.com>
2018-12-06 14:39:40 -0800

    database and list are working

--- assets/javascript/Maria.js
+++ assets/javascript/Maria.js
@@ -1,19 +1,29 @@
 
-
+var list = []
 
 // click on span to delete ingredient
-$("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500,function(){
-        $(this).remove();
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        list.splice(list.indexOf($(this).attr("name")), 1);
+        console.log(list);
+        listDisplay();
     });
     event.stopPropagation();
-    
 });
 
-$("input").keypress(function(event){
- if (event.which === 13){
-    var addIngredient = $(this).val();
-    $(this).val("");
-    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + addIngredient + "</li>")
- }
-});
+$("input").keypress(function (event) {
+    if (event.which === 13) {
+        var addIngredient = $(this).val();
+        list.push(addIngredient);
+        $(this).val("");
+        listDisplay();
+    };
+})
+
+function listDisplay() {
+    $("ul").empty();
+    for (i = 0; i < list.length; i++) {
+        $("ul").append("<li name='" + list[i] + "'><span><i class='far fa-trash-alt'></i></span> " + list[i] + "</li>")
+        console.log(list[i]);
+    }
+}
--- assets/javascript/baylis.js
+++ assets/javascript/baylis.js
@@ -11,14 +11,6 @@ database.ref().on("value", function (snapshot) {
     }
 });
 
-$("input").keypress(function (event) {
-    if (event.which === 13) {
-        var addIngredient = $(this).val().trim();
-        ingredientList.push(addIngredient);
-        console.log(ingredientList);
-    }
-});
-
 $("#submitButton").on("click", function () {
     event.preventDefault();
     for (i = 0; i < ingredientList.length; i++) {
@@ -33,35 +25,4 @@ $("#submitButton").on("click", function () {
     console.log(databaseIngredients)
     database.ref().set(databaseIngredients);
 
-    //     for (i = 0; i < ingredientList.length; i++) {
-    //         if (database.ref(commonIngredients.indexOf(ingredientList[i]) === -1)) {
-    //             database.ref().push({
-    //                 commonIngredients: + ingredientList[i]
-    //             })
-    //         }
-    //         else {
-    //             database.ref().push({
-    //                 commonIngredients: ingredientList[i]++
-    //             })
-    //         }
-    //     }
 });
-
-
-// $("#click-button").on("click", function() {
-//   clickCounter++;
-//   database.ref().set({
-//     clickCount: clickCounter
-//   });
-// });
-
-// // MAIN PROCESS + INITIAL CODE
-// // --------------------------------------------------------------------------------
-
-// database.ref().on("value", function(snapshot) {
-//   console.log(snapshot.val());
-//   $("#click-value").text(snapshot.val().clickCount);
-//   clickCounter = snapshot.val().clickCount;
-// }, function(errorObject) {
-//   console.log("The read failed: " + errorObject.code);
-// });
