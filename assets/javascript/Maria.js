e1c4e047bb6848ef3cd2e94b0b9b5d2565fe6779
Maria Moreno <maria.ibarra87@gmail.com>
2018-12-06 13:33:56 -0800

    working on getting our 2nd api..giphy

--- assets/javascript/Maria.js
+++ assets/javascript/Maria.js
@@ -2,18 +2,38 @@
 
 
 // click on span to delete ingredient
-$("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500,function(){
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
-    
+
 });
 
-$("input").keypress(function(event){
- if (event.which === 13){
-    var addIngredient = $(this).val();
-    $(this).val("");
-    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + addIngredient + "</li>")
- }
+$("input").keypress(function (event) {
+    if (event.which === 13) {
+        var addIngredient = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + addIngredient + "</li>")
+    }
 });
+
+
+
+
+var food = $("#buttonLol").on("click", function (event) {
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=kRsHuhDISov3OLv59PyTyJHBnpNQclEY&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var foodLol = $("#lolButton");
+                $(food).append(foodLol);
+            }
+        });
+});
+
