7e4e92637b12abccd4f0b134a1ac1d68ee2ef1bd
Missy Leathers <missy@avleathers.com>
2018-12-06 13:54:56 -0800

    Add User html and css files

--- assets/javascript/javascriptNewyn.js
+++ assets/javascript/javascriptNewyn.js
@@ -4,40 +4,40 @@ var recipesOut = []
 var edamam_id = "d02d745b";
 var edamam_key = "a27da899f17064b2672f7c38fa09c34e";
 $(document).on("click", "#submitButton", function () {
-    
+
     var ingredientList = $(this).attr();
     databaseIngredients();
-    function databaseIngredients(){
+    function databaseIngredients() {
         event.preventDefault();
         $.ajax({
-        url: "https://api.edamam.com/search?q=" + ingredientList + "&app_id=" + edamam_id + "&app_key=" + edamam_key,
-        method: "GET"
+            url: "https://api.edamam.com/search?q=" + ingredientList + "&app_id=" + edamam_id + "&app_key=" + edamam_key,
+            method: "GET"
         })
-        .then(function(response) {
-// To get image to append into the "Let's get cooking" div
-        // console.log(response);
-        var results = response.hits;
-        // console.log(results[0].recipe.image);
-        // var d = $('<div>');
-        // var images = $('<img>');
-        // images.attr("src", results[0].recipe.image);
-        // d.append(images);
-        // $("#recipeResults").append(d);
-        var results = response.hits;
+            .then(function (response) {
+                // To get image to append into the "Let's get cooking" div
+                // console.log(response);
+                var results = response.hits;
+                // console.log(results[0].recipe.image);
+                // var d = $('<div>');
+                // var images = $('<img>');
+                // images.attr("src", results[0].recipe.image);
+                // d.append(images);
+                // $("#recipeResults").append(d);
+                var results = response.hits;
 
-// For loop to get 10 images into array and append to "Let's get cooking"
-        for(var i = 0; i < results.length; i++){
-            var d = $('<span>');
-            var images = $('<img>');
-            d.addClass('recipeArea');
-            images.attr('src', results[i].recipe.image);
-            d.append(images);
-            $("#recipeResults").append(d);
-            
-        }
-    })
-}
-    });
+                // For loop to get 10 images into array and append to "Let's get cooking"
+                for (var i = 0; i < results.length; i++) {
+                    var d = $('<span>');
+                    var images = $('<img>');
+                    d.addClass('recipeArea');
+                    images.attr('src', results[i].recipe.image);
+                    d.append(images);
+                    $("#recipeResults").append(d);
+
+                }
+            })
+    }
+});
   //$.ajax({
   //  url: ,
     //method: "GET"
