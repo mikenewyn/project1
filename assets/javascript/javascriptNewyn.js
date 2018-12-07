6d0350c85266f0bca5bf83f71121573ec3087554
Michael Nguyen <thinknownotforever@gmail.com>
2018-12-07 09:53:21 -0800

    everything on the recipe api works now

--- assets/javascript/javascriptNewyn.js
+++ assets/javascript/javascriptNewyn.js
@@ -1,15 +1,15 @@
 var ingredientList = []
-
+// Adds user input ingredients to array above.
 $("input").keypress(function (event) {
     if (event.which === 13){
         var addIngredient = $(this).val().trim();
         ingredientList.push(addIngredient);
-
     }
 });
 // app ID and api key for Edamam//
 var edamam_id = "d02d745b";
 var edamam_key = "a27da899f17064b2672f7c38fa09c34e";
+// On click event for getting recipes to append to page.
 $(document).on("click", "#submitButton", function () {
     databaseIngredients();
     function databaseIngredients(){
@@ -29,7 +29,7 @@ $(document).on("click", "#submitButton", function () {
         // $("#recipeResults").append(d);
         var results = response.hits;
 
-// For loop to get 10 images into array and append to "Let's get cooking"
+// For loop to get 10 info bits into array and append to "Let's get cooking"
         for(var i = 0; i < results.length; i++){
             var d = $('<span>');
             var images = $('<img>');
@@ -38,10 +38,15 @@ $(document).on("click", "#submitButton", function () {
             var p = $('<p>');
             images.attr('src', results[i].recipe.image);
             d.addClass('recipeArea');
+            // Gets the image to be a link
             a.attr("href", results[i].recipe.url);
+            //Opens link in new window
             a.attr("target", "_blank");
+            //Adds Title of the recipe.
             p.text(results[i].recipe.label);
+            //Puts label above image
             d.prepend(p);
+            //Adds image to the page.
             d.append(a);
             $("#recipeResults").append(d);
             
@@ -49,17 +54,4 @@ $(document).on("click", "#submitButton", function () {
     })
 }
     });
-  //$.ajax({
-  //  url: ,
-    //method: "GET"
-  //})
-  //.then(function(response) {
-   // console.log(response);
-   // for(var i = 0; i < results.length; i++){
-    //var d = $('<div>');
-    //var image = $('<img>');
-    //d.addClass('recipe-area');
-    //image.attr()
-    //console.log(response.hits[i]);
-    //}
-  //});
+
