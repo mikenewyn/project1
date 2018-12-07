3ddae6c85ad2d3aaba64de9e57c61fb8a0d42995
richard baylis <veecher@gmail.com>
2018-12-07 14:33:06 -0800

    basically done

--- assets/javascript/baylis.js
+++ assets/javascript/baylis.js
@@ -5,22 +5,52 @@ var databaseIngredients = {
     ingredients: {}
 };
 
-database.ref().on("value", function (snapshot) {
-    if (snapshot.val() && snapshot.val().ingredients) {
-        databaseIngredients = snapshot.val();
-    }
-});
+// database.ref().on("value", function (snapshot) {
+//     if (snapshot.val() && snapshot.val().ingredients) {
+//         databaseIngredients = snapshot.val();
+//     }
+// });
 
-$("#submitButton").on("click", function () {
-    event.preventDefault();
-    for (i = 0; i < ingredientList.length; i++) {
-        if (databaseIngredients.ingredients[ingredientList[i]]) {
-            databaseIngredients.ingredients[ingredientList[i]]++;
-        }
-        else {
-            databaseIngredients.ingredients[ingredientList[i]] = 1;
-        }
-    }
-    console.log(databaseIngredients)
-    database.ref().set(databaseIngredients);
-});
+// $("#submitButton").on("click", function () {
+//     event.preventDefault();
+//     for (i = 0; i < ingredientList.length; i++) {
+//         if (databaseIngredients.ingredients[ingredientList[i]]) {
+//             databaseIngredients.ingredients[ingredientList[i]]++;
+//         }
+//         else {
+//             databaseIngredients.ingredients[ingredientList[i]] = 1;
+//         }
+//     }
+//     console.log(databaseIngredients)
+//     database.ref().set(databaseIngredients);
+// });
+
+// function commonIngredientList() {
+//     var commonList = [];
+//     database.ref().on('value', function(snapshot) {
+//         snapshot.forEach(function(childsnapShot){
+//             var ingredientKey = childsnapShot.key;
+//             var ingredientSearched = childsnapShot.val();
+//             commonList.push(ingredientKey + ": " + ingredientSearched);
+//             console.log(commonList);
+//         });
+//     });
+// }
+
+// commonIngredientList();
+
+// commentsRef.on('child_changed', function(data) {
+//     setCommentValues(postElement, data.key, data.val().text, data.val().author);
+//   });
+
+// orderByValue()	
+// var topUserPostsRef = firebase.database().ref('user-posts/' + myUserId).orderByChild('starCount');
+
+// ref.once('value', function(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//       var childKey = childSnapshot.key;
+//       var childData = childSnapshot.val();
+//       // ...
+//     });
+//   });
+  
