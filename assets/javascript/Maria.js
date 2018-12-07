c8d2462efddf7894318f3b41c002386b6c30d7d5
Maria Moreno <maria.ibarra87@gmail.com>
2018-12-06 19:24:39 -0800

    got gif our 2nd api to append to page ... IT WORKSgit add .

--- assets/javascript/Maria.js
+++ assets/javascript/Maria.js
@@ -2,38 +2,40 @@
 
 
 // click on span to delete ingredient
-$("ul").on("click", "span", function (event) {
-    $(this).parent().fadeOut(500, function () {
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     event.stopPropagation();
-
+    
 });
 
-$("input").keypress(function (event) {
-    if (event.which === 13) {
-        var addIngredient = $(this).val();
-        $(this).val("");
-        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + addIngredient + "</li>")
-    }
+$("input").keypress(function(event){
+ if (event.which === 13){
+    var addIngredient = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + addIngredient + "</li>")
+ }
 });
 
 
 
-
-var food = $("#buttonLol").on("click", function (event) {
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=kRsHuhDISov3OLv59PyTyJHBnpNQclEY&limit=10";
+function addFood () {
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=cooking&api_key=kRsHuhDISov3OLv59PyTyJHBnpNQclEY&limit=100";
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .then(function (response) {
-
+            var randomNumber = Math.floor(Math.random() * 100) + 1  
             var results = response.data;
-            for (var i = 0; i < results.length; i++) {
-                var foodLol = $("#lolButton");
-                $(food).append(foodLol);
-            }
+                var foodDiv = $("#food");
+                var foodImage = $("<img>");
+                foodImage.attr("src", results[randomNumber].images.downsized.url);
+                foodDiv.append(foodImage);
         });
-});
+};
 
+$(function() {
+    addFood();
+  });
